0e1369fdb317ac38af6231940319e5608b809c43
Arkady Drovosekov <arkdro@gmail.com>
2012-02-07 20:31:48 +0400

    stat record moved to separate file; stat added to sth record

--- include/smoke_test.hrl
+++ include/smoke_test.hrl
@@ -1,28 +1,31 @@
 -ifndef(smoke_test).
 -define(smoke_test, true).
 
--record(stat, {sum = 0.0, sum_sq = 0.0, count = 0}).
+-include("child.hrl").
+-include("stat.hrl").
 
 -record(child, {
           id              :: reference(),
+          job             :: undefined | #chi{},
           debug = []      :: [atom()],
           timer           :: reference(),
           stat  = #stat{} :: #stat{},
           url             :: string(),
           hz              :: non_neg_integer(),
           seconds         :: non_neg_integer(),
-          cnt             :: non_neg_integer()
+          cnt = 0         :: non_neg_integer()
 }).
 
 % handler's state
 -record(sth, {
-          debug    = [] :: [atom()],
-          children = [] :: [pid()],
-          count         :: non_neg_integer(),
-          url           :: string(),
-          hz            :: non_neg_integer(),
-          seconds       :: non_neg_integer()
-                           }).
+          stat = #stat{} :: #stat{},
+          debug    = []  :: [atom()],
+          children = []  :: [pid()],
+          count          :: non_neg_integer(),
+          url            :: string(),
+          hz             :: non_neg_integer(),
+          seconds        :: non_neg_integer()
+                            }).
 
 -record(st, {
           url     :: string(),
---
+++ include/stat.hrl
@@ -0,0 +1,6 @@
+-ifndef(smoke_stat).
+-define(smoke_stat, true).
+
+-record(stat, {sum = 0.0, sum_sq = 0.0, count = 0}).
+
+-endif.
